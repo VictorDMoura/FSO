8f616cd3e52791b76bcb83a2a145aa62d7118f07
VictorDMoura <mouramvictor@gmail.com>
2026-02-02 15:50:05 -0300

    feat: add morgan middleware for logging HTTP requests

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,13 @@
-express = require("express");
+const morgan = require("morgan");
+const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
+
+morgan.token("body", (req) => JSON.stringify(req.body));
 
 let PERSONS = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
