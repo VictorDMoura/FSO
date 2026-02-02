4c64ea6312fac4fec446290683960883aa6f52f5
VictorDMoura <mouramvictor@gmail.com>
2026-02-02 15:30:35 -0300

    feat: allow deletion of persons from the phonebook

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const PERSONS = [
+let PERSONS = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
   { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
   { id: 3, name: "Dan Abramov", number: "12-43-234345" },
@@ -32,4 +32,10 @@ app.get("/api/persons/:id", (req, res) => {
     return res.status(404).end();
   }
   res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  PERSONS = PERSONS.filter((person) => person.id !== id);
+  res.status(204).end();
 });
